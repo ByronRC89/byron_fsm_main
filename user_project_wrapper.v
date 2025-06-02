c72e8653918ec4b188293c80ec6375a2f50c3a1a
BR.c <159856194+ByronRC89@users.noreply.github.com>
2025-06-01 19:06:40 -0600

    Update user_project_wrapper.v

--- user_project_wrapper.v
+++ user_project_wrapper.v
@@ -1,17 +1,7 @@
 `default_nettype none
 
 module user_project_wrapper (
-    input  wire        wb_clk_i,
-    input  wire        wb_rst_i,
-    input  wire        wbs_stb_i,
-    input  wire        wbs_cyc_i,
-    input  wire        wbs_we_i,
-    input  wire [3:0]  wbs_sel_i,
-    input  wire [31:0] wbs_dat_i,
-    input  wire [31:0] wbs_adr_i,
-    output wire [31:0] wbs_dat_o,
-    output wire        wbs_ack_o,
-    output wire        wbs_err_o,
+
 
     input  wire        clk,
     input  wire        rst_n,
@@ -21,23 +11,13 @@ module user_project_wrapper (
     output wire [7:0]  uio_out,
     output wire [7:0]  uio_oe,
 
-    input  wire [127:0] la_data_in,
-    output wire [127:0] la_data_out,
-    input  wire [127:0] la_oenb,
 
     input  wire [37:0] io_in,
     output wire [37:0] io_out,
     output wire [37:0] io_oeb,
 
-    output wire [2:0] irq
 );
-
-    assign wbs_dat_o = 32'd0;
-    assign wbs_ack_o = 1'b0;
-    assign wbs_err_o = 1'b0;
-
-    assign la_data_out = 128'd0;
-    assign irq = 3'b000;
+   
     assign io_out = 38'd0;
     assign io_oeb = 38'h3FFFFFFFFF;
 
