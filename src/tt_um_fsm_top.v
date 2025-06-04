488b1c27c32a54c33bb3120c22a41368a9a76019
BR.c <159856194+ByronRC89@users.noreply.github.com>
2025-06-03 20:25:55 -0600

    Update tt_um_fsm_top.v

--- src/tt_um_fsm_top.v
+++ src/tt_um_fsm_top.v
@@ -1,8 +1,8 @@
 module tt_um_fsm_top (
     input  clk,
     input  [3:0] ui_in,     // ui_in[0]=Rapido, ui_in[1]=Lento, ui_in[2]=clk_prescaler, ui_in[3]=reset
-    output [2:0] uo_out     // uo_out[0]=out_30, uo_out[1]=out_50, uo_out[2]=out_100
-     input  ena, 
+    output [2:0] uo_out,     // uo_out[0]=out_30, uo_out[1]=out_50, uo_out[2]=out_100
+    input  ena 
 );
 
     wire clk_1Hz;
