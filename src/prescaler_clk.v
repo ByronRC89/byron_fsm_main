6756aa001cb59830aa39b07681dc72d9b0e7157c
BR.c <159856194+ByronRC89@users.noreply.github.com>
2025-06-03 20:06:54 -0600

    Update prescaler_clk.v

--- src/prescaler_clk.v
+++ src/prescaler_clk.v
@@ -1,42 +1,21 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 25.05.2025 19:59:41
-// Design Name: 
-// Module Name: prescaler_clk
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-//**
-//module prescaler_clk (
-//    input  clk_in,      // CLK100MHZ
-//    input  reset,
-//    output clk_out      // Salida de 1 Hz
-//);
-//    parameter integer MAX_COUNT = 50_000_000 - 1;
+module prescaler_clk (
+    input clk_in,
+    input reset,
+    output reg clk_out
+);
+    reg [25:0] contador;
 
-//    wire [25:0] counter = 0;
-
-//    always_ff @(posedge clk_in or posedge reset) begin
-//        if (reset) begin
-//            counter <= 0;
-//            clk_out <= 0;
-//        end else if (counter == MAX_COUNT) begin
-//          counter <= 0;
-//            clk_out <= ~clk_out;
-//       end else begin
-//           counter <= counter + 1;
-//        end
-//  end
-//endmodule  
+    always @(posedge clk_in or posedge reset) begin
+        if (reset) begin
+            contador <= 26'd0;
+            clk_out <= 0;
+        end else begin
+            if (contador == 26'd49_999_999) begin
+                contador <= 0;
+                clk_out <= ~clk_out;
+            end else begin
+                contador <= contador + 1;
+            end
+        end
+    end
+endmodule
