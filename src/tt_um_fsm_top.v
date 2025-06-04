2035bff11cc6bd0697c486cb9345f677b22746af
BR.c <159856194+ByronRC89@users.noreply.github.com>
2025-06-03 20:50:52 -0600

    Update tt_um_fsm_top.v

--- src/tt_um_fsm_top.v
+++ src/tt_um_fsm_top.v
@@ -1,21 +1,31 @@
 module tt_um_fsm_top (
-    input  clk,
-    input  [3:0] ui_in,     // ui_in[0]=Rapido, ui_in[1]=Lento, ui_in[2]=clk_prescaler, ui_in[3]=reset
-    output [2:0] uo_out,     // uo_out[0]=out_30, uo_out[1]=out_50, uo_out[2]=out_100
-    input  ena 
+    input clk,
+    input rst_n,
+    input ena,
+    input [7:0] ui_in,
+    output [7:0] uo_out,
+    input [7:0] uio_in,
+    output [7:0] uio_out,
+    output [7:0] uio_oe
 );
+
+    // Convertir reset activo bajo a activo alto
+    wire reset;
+    assign reset = ~rst_n;
 
     wire clk_1Hz;
 
+    // Prescaler para generar señal lenta
     prescaler_clk prescaler_inst (
         .clk_in(clk),
-        .reset(ui_in[3]),
+        .reset(reset),
         .clk_out(clk_1Hz)
     );
 
+    // FSM de encendido rampa
     arranque_rampa_parcial rampa_inst (
         .clk(clk_1Hz),
-        .reset(ui_in[3]),
+        .reset(reset),
         .Rapido(ui_in[0]),
         .Lento(ui_in[1]),
         .out_30(uo_out[0]),
@@ -23,5 +33,9 @@ module tt_um_fsm_top (
         .out_100(uo_out[2])
     );
 
-endmodule
+    // Salidas no utilizadas
+    assign uo_out[7:3] = 5'b0;
+    assign uio_out = 8'b0;
+    assign uio_oe = 8'b0;
 
+endmodule
